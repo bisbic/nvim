8e6fa013da8470e6bf347dc99e8fb3959c81a3f2
Andres M <andresmoraleslopez@gmail.com>
2023-12-11 13:52:41 -0500

    Update explore mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " " -- easy to reach leader key
 vim.keymap.set("i", "ii", "<Esc>", { noremap = true }) -- need nvim 0.8+
 -- netrw
-vim.keymap.set("n", "-", vim.cmd.Ex, { noremap = true }) -- need nvim 0.8+
+vim.keymap.set("n", "-", vim.cmd.Explore, { noremap = true }) -- need nvim 0.8+
 -- hop
 vim.api.nvim_set_keymap("n", "s", "<cmd>HopChar2AC<CR>", {noremap=false})
 vim.api.nvim_set_keymap("n", "S", "<cmd>HopChar2BC<CR>", {noremap=false})
