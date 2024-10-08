4c1984b9c98c8722971586d178282094f546970b
Andres M <andresmoraleslopez@gmail.com>
2024-10-08 12:48:15 -0500

    Telescope keybindings for hidden and gitignored files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,3 +3,17 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fff', function()
+  builtin.find_files({
+    hidden = true,
+    no_ignore = true,
+    no_ignore_parent = true
+  })
+end, {})
+vim.keymap.set('n', '<leader>fgg', function()
+  builtin.live_grep({
+    additional_args = function(opts)
+      return {"--hidden", "--no-ignore"}
+    end
+  })
+end, {})
