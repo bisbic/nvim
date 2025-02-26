4914a8849329c01b5c403f41e98bda0d4c56be09
Andres M <andresmoraleslopez@gmail.com>
2025-02-25 23:51:24 -0500

    CodeCompanion configs

--- after/plugin/codecompanion.lua
+++ after/plugin/codecompanion.lua
@@ -2,12 +2,35 @@ require("codecompanion").setup({
   strategies = {
     chat = {
       adapter = "copilot",
+      slash_commands = {
+	["file"] = {
+	  -- Location to the slash command in CodeCompanion
+	  callback = "strategies.chat.slash_commands.file",
+	  description = "Select a file using Telescope",
+	  opts = {
+	    provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+	    contains_code = true,
+	  },
+	},
+      },
     },
     inline = {
       adapter = "copilot",
     },
     agent = {
       adapter = "copilot",
+    },
+  },
+  display = {
+    action_palette = {
+      width = 95,
+      height = 10,
+      prompt = "Prompt ", -- Prompt used for interactive LLM calls
+      provider = "telescope", -- default|telescope|mini_pick
+      opts = {
+	show_default_actions = true, -- Show the default actions in the action palette?
+	show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+      },
     },
   },
 })
